2ba3f5a7a9c9df410a9db5c776f7052b190e8697
Dara <830152@cognizant.com>
2020-01-02 17:13:54 +0530

    my first project

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Helloworld{  
     public static void main(String args[]){  
-     System.out.println("Hello World...Hii kunal is my best friend");  
+     System.out.println("kunal is my best friend");  
     }  
 }  
